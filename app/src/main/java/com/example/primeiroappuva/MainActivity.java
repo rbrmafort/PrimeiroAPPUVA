5ed29b7316b98091f1faf90dcbc69eef7963c8ff
rbrmafort <62165481+rbrmafort@users.noreply.github.com>
2023-04-14 16:46:58 -0300

    limitEditText com TextWatcher para limitar máximo de números.

--- app/src/main/java/com/example/primeiroappuva/MainActivity.java
+++ app/src/main/java/com/example/primeiroappuva/MainActivity.java
@@ -3,31 +3,40 @@ package com.example.primeiroappuva;
 import android.content.Context;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText txtVal1, txtVal2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtVal1 =   findViewById(R.id.editTextNumber);
+        txtVal2 =   findViewById(R.id.editTextNumber2);
+
+        limitEditText(txtVal1, 0, 10000);
+        limitEditText(txtVal2, 0, 10000);
     }
 
     public void operar(View view) {
 
-        EditText txtVal1, txtVal2;
         TextView txtResult1, txtResult2, txtResult3, txtResult4;
         double x, y;
 
-        txtVal1 =   findViewById(R.id.editTextNumber);
-        txtVal2 =   findViewById(R.id.editTextNumber2);
         txtResult1   =   findViewById(R.id.textResposta1);
         txtResult2   =   findViewById(R.id.textResposta2);
         txtResult3   =   findViewById(R.id.textResposta3);
@@ -38,11 +47,14 @@ public class MainActivity extends AppCompatActivity {
         txtResult3.setText("");
         txtResult4.setText("");
 
-        if(TextUtils.isEmpty(txtVal1.getText()))
+        String txt1 = String.valueOf(txtVal1.getText());
+        String txt2 = String.valueOf(txtVal2.getText());
+
+        if(TextUtils.isEmpty(txt1) || TextUtils.equals(txt1, "."))
         {
             txtVal1.setError("Digite um número");
 
-        } else if (TextUtils.isEmpty(txtVal2.getText())){
+        } else if (TextUtils.isEmpty(txt2) || TextUtils.equals(txt2, ".")){
 
             txtVal2.setError("Digite um número");
 
@@ -79,6 +91,55 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return super.dispatchTouchEvent(event);
+    }
+
+    public abstract class MinMaxTextWatcher implements TextWatcher {
+        int min, max;
+        public MinMaxTextWatcher(int min, int max) {
+            super();
+            this.min = min;
+            this.max = max;
+        }
+
+    }
+    private void limitEditText(final EditText ed, int min, int max) {
+        ed.addTextChangedListener(new MinMaxTextWatcher(min, max) {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String str = s.toString();
+                float n = 0;
+                try {
+                    n = Float.parseFloat(str);
+                    if (str.equals(max + ".")) {
+                        ed.setText(Integer.toString(max));
+                        Toast.makeText(getApplicationContext(), "Número máximo permitido é " + max, Toast.LENGTH_SHORT).show();
+                        ed.setSelection(2);
+
+                    } else if (n > max) {
+                        ed.setText(Integer.toString(max));
+                        Toast.makeText(getApplicationContext(), "Número máximo permitido é " + max, Toast.LENGTH_SHORT).show();
+                        ed.setSelection(Integer.toString(max).length());
+
+                    } else if (s.charAt(0) == '0' && n >= 1) { // remover 0`s à esquerda
+                        ed.setText(Float.toString(n));
+                        ed.setSelection(Float.toString(n).length());
+                    }
+                }
+                catch(NumberFormatException nfe) {
+
+                }
+            }
+        });
     }
 
 }
